d307f291133f7ec0d388ab4f0386f40861353de2
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-04-07 23:23:20 +0500

    Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -8,7 +9,11 @@ const Home = () => {
                     <h1 className='text-6xl md:text-8xl text-center md:text-left font-serif'>Stay Curious.</h1>
                     <p className='px-2 pt-10 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
                 </div>
-                <button className='mt-10 px-8 py-3 ml-20 rounded-md bg-black text-white'>Explore Articles</button>
+                <Link to='/articles'>
+                    <button className='mt-10 px-8 py-3 ml-20 rounded-md bg-black text-white'>
+                        Explore Articles
+                    </button>
+                </Link>
             </div>
         </div>
     );
