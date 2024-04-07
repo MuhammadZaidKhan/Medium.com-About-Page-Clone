5183751fa82c62bf3df487b1fcc34da16a0bb6e8
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-04-07 23:17:22 +0500

    Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='min-h-screen bg-amber-400'>
-            <div className='flex flex-col pl-16 pt-20 fixed'>
-                <h1 className='text-6xl md:text-8xl text-center md:text-left font-serif'>Stay Curious.</h1>
-                <p className='px-2 pt-10 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+            <div className='fixed'>
+                <div className='flex flex-col pl-16 pt-20'>
+                    <h1 className='text-6xl md:text-8xl text-center md:text-left font-serif'>Stay Curious.</h1>
+                    <p className='px-2 pt-10 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+                </div>
+                <button className='mt-10 px-8 py-3 ml-20 rounded-md bg-black text-white'>Explore Articles</button>
             </div>
-            <button className='mt-10 px-8 py-3 ml-20 rounded-md bg-black text-white inline-block'>Explore Articles</button>
         </div>
     );
 }
