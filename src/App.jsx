d487ed1c30169765a91f51a33653a3e6039f9e91
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-04-07 23:04:29 +0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import About from './Pages/About';
 import ArticlesListPage from './Pages/ArticlesListPage2';
 import ArticlePage from './Pages/Article-Page';
 import Navbar from './Components/Navbar';
+import Bot from './Components/Bot';
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Bot />
       <hr />
       <Routes>
         <Route path='/' element={<Home />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen bg-amber-400 pb-96 md:pb-28'>
-            <h1 className='text-6xl md:text-7xl text-center md:text-left font-serif'>Stay Curious.</h1>
-            <p className='px-2 pl-10 pt-16 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise from writers on any topic.</p>
-            <button className='mt-16 px-8 py-3 rounded-md bg-black text-white'>Explore Articles</button>
+        <div className='min-h-screen bg-amber-400'>
+            <div className='flex flex-col pl-16 pt-20'>
+                <h1 className='text-6xl md:text-8xl text-center md:text-left font-serif'>Stay Curious.</h1>
+                <p className='px-2 pt-10 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+            </div>
+            <button className='mt-10 px-8 py-3 ml-20 rounded-md bg-black text-white inline-block'>Explore Articles</button>
         </div>
-
-
-    )
+    );
 }
 
-export default Home
+export default Home;
