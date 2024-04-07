938f58b4db8c23875bc8afd95ce94ecfac65dd71
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-04-07 23:06:27 +0500

    Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='min-h-screen bg-amber-400'>
-            <div className='flex flex-col pl-16 pt-20'>
+            <div className='flex flex-col pl-16 pt-20 fixed'>
                 <h1 className='text-6xl md:text-8xl text-center md:text-left font-serif'>Stay Curious.</h1>
                 <p className='px-2 pt-10 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
             </div>
