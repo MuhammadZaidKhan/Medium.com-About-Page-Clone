392fd389447da7b4c3443f43232466b1a596ad75
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-04-22 00:37:15 +0500

    some changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
     return (
         <div className='min-h-screen bg-amber-400'>
             <div className='fixed'>
-                <div className='flex flex-col pl-16 pt-20'>
+                <div className='flex flex-col md:pl-16 pt-20'>
                     <h1 className='text-6xl md:text-8xl text-center md:text-left font-serif'>Stay Curious.</h1>
-                    <p className='px-2 pt-10 font-serif text-2xl text-center md:text-left'>Discover stories, thinking, and expertise <br /> from writers on any topic.</p>
+                    <p className='md:px-2 md:w-2/3 pt-10 font-serif px-10 text-2xl md:text-left'>Discover stories, thinking, and expertise from writers on any topic.</p>
                 </div>
                 <Link to='/articles'>
-                    <button className='mt-10 px-8 py-3 ml-20 rounded-md bg-black text-white'>
+                    <button className='mt-10 px-8 py-3 md:ml-20 ml-24 rounded-md bg-black text-white'>
                         Explore Articles
                     </button>
                 </Link>
